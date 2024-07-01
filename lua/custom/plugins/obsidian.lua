e1c0b99d6965d615f0e86bce965e33848111c924
laztaxon <cello@cello.design>
2024-07-01 15:59:25 -0400

    07-01-2024-4pm

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -25,15 +25,50 @@ return {
           path = '~/Documents/cello-main/',
         },
       },
+      templates = {
+        folder = '~/Documents/cello-main/Templates/',
+        date_format = '%Y-%m-%d-%a',
+        time_format = '%H:%M',
+        substitutions = {
+          today = function()
+            return os.date '%Y-%m-%d'
+          end,
+          yesterday = function()
+            return os.date('%Y-%m-%d', os.time() - 86400)
+          end,
+          now = function()
+            return os.date '%Y-%m-%d-%H:%M'
+          end,
+          time = function()
+            return os.date '%H:%M'
+          end,
+          hour = function()
+            return os.date('%H:%M', os.time() + 3600)
+          end,
+          half = function()
+            return os.date('%H:%M', os.time() + 1800)
+          end,
+          block = function()
+            return os.date('%H:%M', os.time() + (3600 * 1.5))
+          end,
+          hour_start = function()
+            return os.date '%H:00'
+          end,
+          hour_end = function()
+            return os.date('%H:00', os.time() + 3600)
+          end,
+          half_end = function()
+            return os.date('%H:30', os.time() + 3600)
+          end,
+        },
+      },
       daily_notes = {
         -- Optional, if you keep daily notes in a separate directory.
         folder = 'Dailies/',
         -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = '%m-%d-%Y',
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = '%B %-d, %Y',
+        date_format = '%Y-%m-%d',
         -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = 'Dailies/Daily-Template.md',
+        template = '~/Documents/cello-main/Dailies/Daily-Template.md',
       },
       -- Where to put new notes. Valid options are
       -- * "current_dir" - put new notes in same directory as the current buffer.
@@ -60,8 +95,13 @@ return {
       -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log levels defined by "vim.log.levels.*".
       log_level = vim.log.levels.INFO,
       --Function Keymaps
-      vim.keymap.set('v', '<leader>n', vim.cmd.ObsidianExtractNote, { desc = 'Creates new note & links it to the Selected text' }),
-      vim.keymap.set('v', '<leader>l', vim.cmd.ObsidianLink, { desc = 'Links Selected text to a note' }),
+      vim.keymap.set('n', '<leader>zn', vim.cmd.ObsidianSearch, { desc = 'Search or create new note' }),
+      vim.keymap.set('v', '<leader>ze', vim.cmd.ObsidianExtractNote, { desc = 'Creates new note & links it to the Selected text' }),
+      vim.keymap.set('n', '<leader>zt', vim.cmd.ObsidianTemplate, { desc = 'Apply template to current note' }),
+      vim.keymap.set('n', '<leader>zd', vim.cmd.ObsidianToday, { desc = 'Creates new note for today or travel to today' }),
+      vim.keymap.set('n', '<leader>zy', vim.cmd.ObsidianYesterday, { desc = 'Creates new note for yesterday or travel to yesterday' }),
+      vim.keymap.set('n', '<leader>zm', vim.cmd.ObsidianTomorrow, { desc = 'Creates new note for tomorrow or travel to tomorrow' }),
+      vim.keymap.set('v', '<leader>zl', vim.cmd.ObsidianLink, { desc = 'Links Selected text to a note' }),
       -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
       completion = {
         -- Set to false to disable completion.
--- lua/custom/plugins/popup_ui.lua
+++ lua/custom/plugins/popup_ui.lua
@@ -3,16 +3,16 @@ return {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
-      messages = {
-        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-        -- This is a current Neovim limitation.
-        enabled = true, -- enables the Noice messages UI
-        view = 'mini', -- default view for messages
-        view_error = 'mini', -- view for errors
-        view_warn = 'mini', -- view for warnings
-        view_history = 'mini', -- view for :messages
-        view_search = 'mini', -- view for search count messages. Set to `false` to disable
-      },
+      -- messages = {
+      --   -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+      --   -- This is a current Neovim limitation.
+      --   enabled = true, -- enables the Noice messages UI
+      --   view = 'mini', -- default view for messages
+      --   view_error = 'mini', -- view for errors
+      --   view_warn = 'mini', -- view for warnings
+      --   view_history = 'mini', -- view for :messages
+      --   view_search = 'mini', -- view for search count messages. Set to `false` to disable
+      -- },
       notify = {
         -- Noice can be used as `vim.notify` so you can route any notification like other messages
         -- Notification messages have their level and other properties set.
